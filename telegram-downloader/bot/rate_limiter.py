ae179d39cffdf3e2cd37cf31f6c32853f9686173
lucabravi <luca.bravi@yahoo.it>
2025-03-27 15:48:46 +0100

    backup

--- telegram-downloader/bot/rate_limiter.py
+++ telegram-downloader/bot/rate_limiter.py
@@ -1,29 +1,67 @@
 import asyncio
 import logging
 import time
+from datetime import datetime
 
 from pyrogram.errors import FloodWait
 
+last_messages = []
+last_block = datetime.now()
+last_block_seconds = 0
 
 async def catch_rate_limit(function, wait=True, *args, **kwargs):
+    global last_block
+    global last_block_seconds
     while True:
+        if not wait and (datetime.now() - last_block).total_seconds() <= last_block_seconds + 2:
+            return None
+        if wait and len(last_messages) >= 5 and (datetime.now() - last_messages[-5]).total_seconds() <= 1:
+            await asyncio.sleep(2)
+            continue
+        elif not wait and len(last_messages) >= 5 and (datetime.now() - last_messages[-5]).total_seconds() <= 1:
+            return None
+
+        last_messages.append(datetime.now())
+        if len(last_messages) > 5:
+            last_messages.pop(0)
+
+
         try:
             av = await function(*args, **kwargs)
             return av
         except FloodWait as e:
-            logging.warning(f'catch_rate_limit - {e}')
+            logging.warning(f'async catch_rate_limit - {e}')
+            last_block = datetime.now()
+            last_block_seconds = e.value
             if not wait:
+                asyncio.sleep(2)
                 break
             await asyncio.sleep(e.value)
 
 
 def sync_catch_rate_limit(function, wait=True, *args, **kwargs):
+    global last_block
+    global last_block_seconds
     while True:
+        if not wait and (datetime.now() - last_block).total_seconds() <= last_block_seconds + 2:
+            return None
+        if wait and len(last_messages) >= 5 and (datetime.now() - last_messages[-5]).total_seconds() <= 1:
+            time.sleep(2)
+            continue
+        elif not wait and len(last_messages) >= 5 and (datetime.now() - last_messages[-5]).total_seconds() <= 1:
+            return None
+
+        last_messages.append(datetime.now())
+        if len(last_messages) > 5:
+            last_messages.pop(0)
+
         try:
             av = function(*args, **kwargs)
             return av
         except FloodWait as e:
-            logging.warning(f'catch_rate_limit - {e}')
+            logging.warning(f'sync catch_rate_limit - {e}')
+            last_block = datetime.now()
+            last_block_seconds = e.value
             if not wait:
                 break
             time.sleep(e.value)
